934a601c7690c5ba70ef6e287e4825df314f38b5
LBKhoa <152173511+LBKhoa@users.noreply.github.com>
2024-05-22 19:47:22 +0700

    Dashboard Food App UI ( Crate BestFoodAdapter, CategoryAdapter, conect with layout)

--- FoodAppOder/app/src/main/java/khoalb/ntu/foodappoder/Activity/MainActivity.java
+++ FoodAppOder/app/src/main/java/khoalb/ntu/foodappoder/Activity/MainActivity.java
@@ -1,6 +1,7 @@
 package khoalb.ntu.foodappoder.Activity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 
 import androidx.activity.EdgeToEdge;
@@ -9,15 +10,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
+import khoalb.ntu.foodappoder.Adapter.BestFoodsAdapter;
+import khoalb.ntu.foodappoder.Adapter.CategoryAdapter;
+import khoalb.ntu.foodappoder.Domain.Category;
+import khoalb.ntu.foodappoder.Domain.Foods;
 import khoalb.ntu.foodappoder.Domain.Location;
+import khoalb.ntu.foodappoder.Domain.Price;
+import khoalb.ntu.foodappoder.Domain.Time;
 import khoalb.ntu.foodappoder.R;
 import khoalb.ntu.foodappoder.databinding.ActivityMainBinding;
 
@@ -37,6 +48,64 @@ public class MainActivity extends BaseActivity {
         });
 
         initLocation();
+        initTime();
+        initPrice();
+        initBestFood();
+        initCategory();
+    }
+
+    private void initBestFood() {
+        DatabaseReference myRef = database.getReference("Foods");
+        binding.progressBarBestFood.setVisibility(View.VISIBLE);
+        ArrayList<Foods> list = new ArrayList<>();
+        Query query = myRef.orderByChild("BestFood").equalTo(true);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    for (DataSnapshot issue: snapshot.getChildren()){
+                        list.add(issue.getValue(Foods.class));
+                    }
+                    if (list.size()>0){
+                        binding.bestFoodView.setLayoutManager(new LinearLayoutManager(MainActivity.this,LinearLayoutManager.HORIZONTAL,false));
+                        RecyclerView.Adapter adapter = new BestFoodsAdapter(list);
+                        binding.bestFoodView.setAdapter(adapter);
+                    }
+                    binding.progressBarBestFood.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+    private void initCategory() {
+        DatabaseReference myRef = database.getReference("Category");
+        binding.progressBarCategory.setVisibility(View.VISIBLE);
+        ArrayList<Category> list = new ArrayList<>();
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    for (DataSnapshot issue: snapshot.getChildren()){
+                        list.add(issue.getValue(Category.class));
+                    }
+                    if (list.size()>0){
+                        binding.categoryView.setLayoutManager(new GridLayoutManager(MainActivity.this,4));
+                        RecyclerView.Adapter adapter = new CategoryAdapter(list);
+                        binding.categoryView.setAdapter(adapter);
+                    }
+                    binding.progressBarCategory.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     private void initLocation() {
@@ -53,6 +122,52 @@ public class MainActivity extends BaseActivity {
                     ArrayAdapter<Location>  adapter = new ArrayAdapter<>(MainActivity.this, R.layout.sp_item,list);
                     adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                     binding.locationSp.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+    private void initTime() {
+        DatabaseReference myRef = database.getReference("Time");
+        ArrayList<Time> list = new ArrayList<>();
+
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    for (DataSnapshot issue: snapshot.getChildren()){
+                        list.add(issue.getValue(Time.class));
+                    }
+                    ArrayAdapter<Time>  adapter = new ArrayAdapter<>(MainActivity.this, R.layout.sp_item,list);
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    binding.timeSp.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+    private void initPrice() {
+        DatabaseReference myRef = database.getReference("Price");
+        ArrayList<Price> list = new ArrayList<>();
+
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    for (DataSnapshot issue: snapshot.getChildren()){
+                        list.add(issue.getValue(Price.class));
+                    }
+                    ArrayAdapter<Price>  adapter = new ArrayAdapter<>(MainActivity.this, R.layout.sp_item,list);
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    binding.priceSp.setAdapter(adapter);
                 }
             }
 
