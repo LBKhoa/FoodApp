7da5412aaf76955f21bf89f294a06c91354ea279
LBKhoa <152173511+LBKhoa@users.noreply.github.com>
2024-05-22 13:19:04 +0700

    create class Category, Foods, Location, Price, Time and set JSON Firebase database

---
+++ FoodAppOder/app/src/main/java/khoalb/ntu/foodappoder/Domain/Foods.java
@@ -0,0 +1,129 @@
+package khoalb.ntu.foodappoder.Domain;
+
+public class Foods {
+    private int CategoryId;
+    private String Description;
+    private boolean BestFood;
+    private int Id;
+    private int locationId;
+    private double Price;
+    private String ImagePath;
+    private int PriceId;
+    private double Star;
+    private int timeId;
+    private int timeValue;
+    private String Title;
+    private int numberInCart;
+
+    public Foods() {
+    }
+
+    @Override
+    public String toString() {
+        return   Title;
+    }
+
+    public int getCategoryId() {
+        return CategoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        CategoryId = categoryId;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public boolean isBestFood() {
+        return BestFood;
+    }
+
+    public void setBestFood(boolean bestFood) {
+        BestFood = bestFood;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public int getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(int locationId) {
+        this.locationId = locationId;
+    }
+
+    public double getPrice() {
+        return Price;
+    }
+
+    public void setPrice(double price) {
+        Price = price;
+    }
+
+    public String getImagePath() {
+        return ImagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        ImagePath = imagePath;
+    }
+
+    public int getPriceId() {
+        return PriceId;
+    }
+
+    public void setPriceId(int priceId) {
+        PriceId = priceId;
+    }
+
+    public double getStar() {
+        return Star;
+    }
+
+    public void setStar(double star) {
+        Star = star;
+    }
+
+    public int getTimeId() {
+        return timeId;
+    }
+
+    public void setTimeId(int timeId) {
+        this.timeId = timeId;
+    }
+
+    public int getTimeValue() {
+        return timeValue;
+    }
+
+    public void setTimeValue(int timeValue) {
+        this.timeValue = timeValue;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public int getNumberInCart() {
+        return numberInCart;
+    }
+
+    public void setNumberInCart(int numberInCart) {
+        this.numberInCart = numberInCart;
+    }
+}
